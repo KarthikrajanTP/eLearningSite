79a82e3bd13937232038ebb46585d8987a05e4f4
KarthikrajanTP <karthikrajan@testpress.in>
2024-07-26 11:53:18 +0000

    setup nginx

---
+++ educa/settings/pro.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+ ('Antonio M', 'email@mydomain.com'),
+)
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'educa',
+       'USER': 'educa',
+       'PASSWORD': 'admin',
+   }
+}
+
+# # SSL config
+# SECURE_SSL_REDIRECT = True
+# CSRF_COOKIE_SECURE = True
